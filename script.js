0f750e4ae06506856f95e69ff20588d4add70d4c
Theo <45401519+Tchoow@users.noreply.github.com>
2022-05-18 09:16:10 +0200

    Clean js

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ let robots = document.querySelectorAll(".robot");
 
 // init
 function initPositionRobots() {
-  for ( let r of robots ) {
-    let top  = Math.floor(Math.random() *  window.innerHeight-50)+25;
-    let left = Math.floor(Math.random() *  window.innerWidth-50 )+25;
-    r.style.left = left + "px"
-    r.style.top  = top  + "px"
-  }
+    for (let r of robots) {
+        let top  = Math.floor(Math.random() * window.innerHeight - 50) + 25;
+        let left = Math.floor(Math.random() * window.innerWidth - 50)  + 25;
+        r.style.left = left + "px"
+        r.style.top  = top  + "px"
+    }
 }
 
 // get robot position
 function getRobotPosition(robot) {
-  let coords = []
-  coords[0]  =  (robot.style.left).substring(0, robot.style.left.length-2);
-  coords[1]  =  (robot.style.top ).substring(0, robot.style.top.length-2 );
-  return coords;
+    let coords = []
+    coords[0]  = (robot.style.left).substring(0, robot.style.left.length - 2);
+    coords[1]  = (robot.style.top).substring(0, robot.style.top.length - 2);
+    return coords;
 }
 
 // sleep delay
@@ -25,34 +25,33 @@ function sleep(delay) {
     while (new Date().getTime() < start + delay);
 }
 
-// align robots
-function alignRobots(robots) {
-  console.log("test");
-}
-
 //function delete current robot from array
 function deleteRobotFromListOfRobot(robot, lstRobot) {
-  for (let rob of lstRobot) {
-    console.log(rob);
-  }
+    for (let rob of lstRobot) {
+        if (rob == robot) {
+            lstRobot.splice(lstRobot, lstRobot.indexOf(rob));
+        }
+    }
+    return lstRobot;
 }
 
-function getTwoClosestRobots(robot)  {
+function getPositionToGo(robot) {
+    // tmp variables
     let tempListRobots = [];
     let twoClosest     = [];
     let goalX          = getRobotPosition(robot)[0];
     let goalY          = getRobotPosition(robot)[1];
-    
+
     // get all robots in temp array
-    for ( let r of robots ) { tempListRobots.push(getRobotPosition(r)[0]) }
-    
+    for (let r of robots) { tempListRobots.push(getRobotPosition(r)[0]) }
+
     // console.log(getRobotPosition(r)[0])
-    
+
     // get the near X
-    for(let cpt = 0; cpt < tempListRobots.length-1; cpt++) {
-        tempListRobots[cpt] =  parseInt(Math.abs(goalX - tempListRobots[cpt]));
+    for (let cpt = 0; cpt < tempListRobots.length - 1; cpt++) {
+        tempListRobots[cpt] = parseInt(Math.abs(goalX - tempListRobots[cpt]));
     }
-     
+
     tempListRobots.sort();
     console.log(tempListRobots);
 }
@@ -60,13 +59,5 @@ function getTwoClosestRobots(robot)  {
 // init
 initPositionRobots();
 console.log(getRobotPosition(robots[0]));
-getTwoClosestRobots(robots[0]);
-deleteRobotFromListOfRobot
-
-/*
-while (true) {
-  alignRobots(robots);
-  sleep(1000);
-}
-*/
-//console.log(robots)
+getPositionToGo(robots[0]);
+deleteRobotFromListOfRobot(robots[0], robots);
