55b7713714a39f8a6d889d82d3d4acbb504f78ae
Theo <theo.lavie@etu.univ-lehavre.fr>
2022-05-18 13:43:20 +0200

    Coord X fonctionne

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ let robots = document.querySelectorAll(".robot");
 // init
 function initPositionRobots() {
     for (let r of robots) {
-        let top  = Math.floor(Math.random() * window.innerHeight - 50) + 25;
-        let left = Math.floor(Math.random() * window.innerWidth - 50)  + 25;
+        let top = Math.floor(Math.random() * window.innerHeight - 50) + 25;
+        let left = Math.floor(Math.random() * window.innerWidth - 50) + 25;
         r.style.left = left + "px"
-        r.style.top  = top  + "px"
+        r.style.top = top + "px"
     }
 }
 
 // get robot position
 function getRobotPosition(robot) {
     let coords = []
-    coords[0]  = (robot.style.left).substring(0, robot.style.left.length - 2);
-    coords[1]  = (robot.style.top).substring(0, robot.style.top.length - 2);
+    coords[0] = (robot.style.left).substring(0, robot.style.left.length - 2);
+    coords[1] = (robot.style.top).substring(0, robot.style.top.length - 2);
     return coords;
 }
 
@@ -27,37 +27,93 @@ function sleep(delay) {
 
 //function delete current robot from array
 function deleteRobotFromListOfRobot(robot, lstRobot) {
-    for (let rob of lstRobot) {
-        if (rob == robot) {
-            lstRobot.splice(lstRobot, lstRobot.indexOf(rob));
-        }
-    }
-    return lstRobot;
+
+    // cast to arrray to get fonctions
+    let tempLstRobot = Array.from(lstRobot);
+    let index = tempLstRobot.indexOf(robot);
+    tempLstRobot.splice(index, 1);
+    return tempLstRobot;
 }
 
+function compareCoord(a, b) {
+    if (a[1] < b[1]) { return -1; }
+    if (a[1] > b[1]) { return 1; }
+    return 0;
+}
+
+
+function ultimeCompareCoord(a, b) {
+    
+}
+
+
+
 function getPositionToGo(robot) {
+
     // tmp variables
     let tempListRobots = [];
-    let twoClosest     = [];
-    let goalX          = getRobotPosition(robot)[0];
-    let goalY          = getRobotPosition(robot)[1];
+    let tempXmoreNear  = [];
+    let tempYmoreNear  = [];
+    let goalX = getRobotPosition(robot)[0];
+    let goalY = getRobotPosition(robot)[1];
+
 
     // get all robots in temp array
-    for (let r of robots) { tempListRobots.push(getRobotPosition(r)[0]) }
+    for (let r of robots) { tempListRobots.push(r) }
+    // remove current robot from this Array
+    tempListRobots = Array.from(deleteRobotFromListOfRobot(robot, tempListRobots));
 
-    // console.log(getRobotPosition(r)[0])
 
-    // get the near X
-    for (let cpt = 0; cpt < tempListRobots.length - 1; cpt++) {
-        tempListRobots[cpt] = parseInt(Math.abs(goalX - tempListRobots[cpt]));
+    // fill array with x coord
+    for (let cpt = 0; cpt < tempListRobots.length; cpt++) {
+        tempXmoreNear[cpt] = new Array(cpt,
+            parseInt((tempListRobots[cpt].style.left)
+                .substring(0, (tempListRobots[cpt].style.left).length - 2)));
     }
 
-    tempListRobots.sort();
-    console.log(tempListRobots);
+    let newTempX = [];
+    for (let cpt = 0; cpt < tempListRobots.length; cpt++) {
+        newTempX[cpt] = new Array(cpt, Math.abs(parseInt(goalX - tempXmoreNear[cpt][1])));
+    }
+
+
+    newTempX.sort(compareCoord);
+
+
+    // fill array with y coord
+    for (let cpt = 0; cpt < tempListRobots.length; cpt++) {
+        tempYmoreNear[cpt] = new Array(cpt,
+            parseInt((tempListRobots[cpt].style.top)
+                .substring(0, (tempListRobots[cpt].style.top).length - 2)));
+    }
+
+    let newTempY = [];
+    for (let cpt = 0; cpt < tempListRobots.length; cpt++) {
+        newTempY[cpt] = new Array(cpt, Math.abs(parseInt(goalY - tempYmoreNear[cpt][1])));
+    }
+
+    newTempY.sort(compareCoord);
+
+    let newTempXY = [];
+    for (let cpt = 0; cpt < tempListRobots.length; cpt++) {
+        newTempXY[cpt] = new Array(cpt, newTempX[0], newTempY[0]);
+    }
+
+    //newTempXY.sort(ultimeCompareCoord);
+
+    console.log(newTempXY);
+    
+    robot.style.height = "50px";
+    robot.style.width  = "50px";
+    robot.style.border = "5px solid yellow";
+    tempListRobots[newTempX[0][0]].style.height = "50px";
+    tempListRobots[newTempX[0][0]].style.width  = "50px";
+    tempListRobots[newTempX[1][0]].style.height = "50px";
+    tempListRobots[newTempX[1][0]].style.width  = "50px";
+    tempListRobots[newTempX[0][0]].style.height = "50px";
+    tempListRobots[newTempX[0][0]].style.width  = "50px";
 }
 
 // init
 initPositionRobots();
-console.log(getRobotPosition(robots[0]));
 getPositionToGo(robots[0]);
-deleteRobotFromListOfRobot(robots[0], robots);
